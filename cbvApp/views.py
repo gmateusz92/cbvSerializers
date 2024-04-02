5eb975c216208edde6bc2627e56c07a87871c708
Mateusz <gmateusz92@gmail.com>
2024-04-02 21:08:03 +0200

    1

--- cbvApp/views.py
+++ cbvApp/views.py
@@ -6,16 +6,50 @@ from .serializers import StudentSerializer
 from django.http import Http404
 
 from rest_framework import generics, mixins
+from rest_framework import viewsets
 
-class StudentList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class StudentViewSet(viewsets.ModelViewSet): #non pk and pk operations
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def get(self,request):
-        return self.list(request)
+
+# class StudentList(generics.ListCreateAPIView): #non pk operations
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+
+# class StudentDetail(generics.RetrieveUpdateDestroyAPIView): #pk operations
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+
+
+
+# class StudentList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+#     def get(self,request):
+#         return self.list(request)
     
-    def post(self,request):
-        return self.create(request)
+#     def post(self,request):
+#         return self.create(request)
+    
+# class StudentDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+#     def get(self,request,pk):
+#         return self.retrieve(request,pk)
+    
+#     def put(self,request,pk):
+#         return self.update(request, pk)
+    
+#     def delete(self,request,pk):
+#         return self.destroy(request,pk)
+
+
+
 
 
 # class StudentList(APIView):
